9287385b0d67afa0bf9f6464fb5998b0bb14a0a9
tosankrisna <tosankrisna@gmail.com>
2020-06-24 01:27:24 +0800

    change service-worker.js location in register.js

--- js/register.js
+++ js/register.js
@@ -2,7 +2,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
         navigator.serviceWorker
-            .register('../service-worker.js')
+            .register('/service-worker.js')
             .then(function() {
                 console.log('Pendaftaran ServiceWorker berhasil');
             })
